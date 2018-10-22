ca0413b39ca73f22c098bff40bc0fce82b473821
liangchaoGitHub <44359279+liangchaoGitHub@users.noreply.github.com>
2018-10-22 16:48:21 +0800

    测试

---
+++ src/test_git_project/TestGit.java
@@ -0,0 +1,14 @@
+package test_git_project;
+
+public class TestGit {
+
+	public static void main(String[] arg) {
+		System.out.println("Test Git");
+		System.out.println("Test Git2");
+		System.out.println("Test Git3");
+		System.out.println("Test Git3");
+		System.out.println("Test Git3");
+		System.out.println("Test Git3");
+		System.out.println("Test Git3");
+	}
+}
